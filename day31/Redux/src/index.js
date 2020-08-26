4985c7823c9b39cdb23af2288762b0cc3835aaff
karthickjaya <65964144+karthickjaya@users.noreply.github.com>
2020-08-26 19:59:30 +0530

    Redux,Thunk,Saga

---
+++ day31/Redux/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reducer from "./store/reducer/reducer";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
